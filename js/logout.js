46aedbdc01ecd297981fd10b6a10d999386776cb
Titus Rangga <titusrangga142@gmail.com>
2025-07-28 04:07:15 +0700

    fix: fix the button function to logout, feat: add dropdown function to bring up logout menu

--- js/logout.js
+++ js/logout.js
@@ -1,4 +1,4 @@
-const logOut = document.querySelector(".dropdown-menu > div:nth-child(1) > button");
+const logOut = document.querySelector(".log-out");
 
 logOut.addEventListener("click", function ()  {
     localStorage.removeItem('User');
@@ -6,3 +6,27 @@ logOut.addEventListener("click", function ()  {
 
 }
 )
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const button = document.querySelector('.dropdown > button');
+    const menu = document.querySelector('.dropdown-menu');
+
+
+    function toggleMenu () {
+            menu.classList.toggle('hidden');
+        }
+        // Event listener untuk hamburger button
+        button.addEventListener('click', function(e) {
+            toggleMenu();
+        });
+
+        // Tutup menu saat klik di luar
+        document.addEventListener('click', function(e) {
+            if (!button.contains(e.target) && !menu.contains(e.target)) {
+                menu.classList.remove('active');
+            }
+        });
+}   
+
+);
